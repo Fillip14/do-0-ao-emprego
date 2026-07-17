5d1e4c4cc50e745bdf31735ee8e1f6b4dc66937e
Fillip <fillip.apol@gmail.com>
2026-07-17 17:35:30 -0300

    Atualizacao do server.js. Criado collections para utilizar o bruno. devlog atualizado.

--- etapas/etapa-2/api/server.js
+++ etapas/etapa-2/api/server.js
@@ -4,15 +4,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.disable('x-powered-by');
 const tasks = [];
+
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  console.log(req.headers);
+  next();
+});
 
 app.get('/tasks', (req, res) => {
   res.json(tasks);
 });
 
 app.post('/tasks', (req, res) => {
-  const newTask = { id: crypto.randomUUID(), title: req.body.titulo };
+  const newTask = { id: crypto.randomUUID(), title: req.body.title };
   tasks.push(newTask);
   res.status(201).json(newTask);
 });
--- etapas/etapa-2/devlog-etapa-2.md
+++ etapas/etapa-2/devlog-etapa-2.md
@@ -1,7 +1,7 @@
 # Devlog — Etapa 2 (Back-end: Node, Express, TypeScript e banco)
 
-##
+## 17/07/2026
 
-- O que aprendi:
+- O que aprendi: npm init, npm start, npm run dev, criar um servidor simples em node. dependecies e devDependecies. Utilizar o collections do Bruno.
 - Travei/faltou:
 - Amanhã:
