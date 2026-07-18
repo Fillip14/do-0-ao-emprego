7f38a9855ef423496dc6f3f3836980efd48c3ad0
Fillip <fillip.apol@gmail.com>
2026-07-18 10:31:59 -0300

    Criado middleware de pagina nao encontrada, erros inesperados e da validação do title.

--- etapas/etapa-2/api/server.js
+++ etapas/etapa-2/api/server.js
@@ -12,6 +12,15 @@ app.use((req, res, next) => {
   next();
 });
 
+const validateTitle = (req, res, next) => {
+  const title = req.body.title;
+
+  if (typeof title !== 'string' || title.trim() === '')
+    return res.status(400).json({ message: 'Invalid title' });
+
+  next();
+};
+
 app.get('/tasks', (req, res) => res.json(tasks));
 
 app.get('/tasks/:id', (req, res) => {
@@ -19,32 +28,26 @@ app.get('/tasks/:id', (req, res) => {
 
   const task = tasks.find((task) => task.id === taskId);
 
-  if (typeof task === 'undefined') return res.status(404).json('Task não encontrada');
+  if (typeof task === 'undefined') return res.status(404).json({ message: 'Task not found' });
 
   return res.status(200).json(task);
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', validateTitle, (req, res) => {
   const title = req.body.title;
-
-  if (typeof title !== 'string' || title.trim() === '')
-    return res.status(400).json('Titulo inválido');
 
   const newTask = { id: crypto.randomUUID(), title: title };
   tasks.push(newTask);
   return res.status(201).json(newTask);
 });
 
-app.patch('/tasks/:id', (req, res) => {
+app.patch('/tasks/:id', validateTitle, (req, res) => {
   const taskId = req.params.id;
   const newTitle = req.body.title;
 
-  if (typeof newTitle !== 'string' || newTitle.trim() === '')
-    return res.status(400).json('Titulo inválido');
-
   const task = tasks.find((task) => task.id === taskId);
 
-  if (typeof task === 'undefined') return res.status(404).json('Task não encontrada');
+  if (typeof task === 'undefined') return res.status(404).json({ message: 'Task not found' });
 
   task.title = newTitle;
 
@@ -56,11 +59,20 @@ app.delete('/tasks/:id', (req, res) => {
 
   const posTask = tasks.findIndex((task) => task.id === taskId);
 
-  if (posTask === -1) return res.status(404).json('Task não encontrada');
+  if (posTask === -1) return res.status(404).json({ message: 'Task not found' });
 
   tasks.splice(posTask, 1);
 
-  return res.status(200).json({ message: 'Removido' });
+  return res.status(200).json({ message: 'Removed' });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status || 500).json({ message: 'An unexpected error occurred' });
 });
 
 app.listen(port, () => {
