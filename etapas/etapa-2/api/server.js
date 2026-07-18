390f276309f9df9530d57e5e4a156cfd093dcf56
Fillip <fillip.apol@gmail.com>
2026-07-18 10:02:14 -0300

    Adicionado mais rotas no server.js e mais req no bruno

--- etapas/etapa-2/api/server.js
+++ etapas/etapa-2/api/server.js
@@ -9,18 +9,58 @@ const tasks = [];
 
 app.use((req, res, next) => {
   console.log(req.method, req.url);
-  console.log(req.headers);
   next();
 });
 
-app.get('/tasks', (req, res) => {
-  res.json(tasks);
+app.get('/tasks', (req, res) => res.json(tasks));
+
+app.get('/tasks/:id', (req, res) => {
+  const taskId = req.params.id;
+
+  const task = tasks.find((task) => task.id === taskId);
+
+  if (typeof task === 'undefined') return res.status(404).json('Task não encontrada');
+
+  return res.status(200).json(task);
 });
 
 app.post('/tasks', (req, res) => {
-  const newTask = { id: crypto.randomUUID(), title: req.body.title };
+  const title = req.body.title;
+
+  if (typeof title !== 'string' || title.trim() === '')
+    return res.status(400).json('Titulo inválido');
+
+  const newTask = { id: crypto.randomUUID(), title: title };
   tasks.push(newTask);
-  res.status(201).json(newTask);
+  return res.status(201).json(newTask);
+});
+
+app.patch('/tasks/:id', (req, res) => {
+  const taskId = req.params.id;
+  const newTitle = req.body.title;
+
+  if (typeof newTitle !== 'string' || newTitle.trim() === '')
+    return res.status(400).json('Titulo inválido');
+
+  const task = tasks.find((task) => task.id === taskId);
+
+  if (typeof task === 'undefined') return res.status(404).json('Task não encontrada');
+
+  task.title = newTitle;
+
+  return res.status(200).json(task);
+});
+
+app.delete('/tasks/:id', (req, res) => {
+  const taskId = req.params.id;
+
+  const posTask = tasks.findIndex((task) => task.id === taskId);
+
+  if (posTask === -1) return res.status(404).json('Task não encontrada');
+
+  tasks.splice(posTask, 1);
+
+  return res.status(200).json({ message: 'Removido' });
 });
 
 app.listen(port, () => {
