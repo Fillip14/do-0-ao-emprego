6ea962eaafa396f1cd3055c09dfe0fc92336e6ff
Fillip <fillip.apol@gmail.com>
2026-07-24 13:40:30 -0300

    Campo term agora é obrigatorio. Criado type guard pro patch

--- etapas/etapa-2/api/src/tasks.ts
+++ etapas/etapa-2/api/src/tasks.ts
@@ -5,8 +5,10 @@ export interface Task {
   readonly id: number;
   title: string;
   status: 'todo' | 'doing' | 'done';
-  term?: string;
+  term: string | null;
 }
+
+const allowedFields = ['title', 'status', 'term'];
 
 // export interface Task extends NewTask {
 //   readonly id: number;
@@ -15,7 +17,7 @@ export interface Task {
 // const STATUSES = ['todo', 'doing', 'done'] as const;
 
 export type NewTask = Omit<Task, 'id'>;
-// export type TaskPatch = Partial<Omit<Task, 'id'>>;
+export type TaskPatch = Partial<Omit<Task, 'id'>>;
 // export type TaskPreview = Pick<Task, 'id' | 'title'>;
 
 export const isNewTask = (body: unknown): body is NewTask => {
@@ -23,16 +25,47 @@ export const isNewTask = (body: unknown): body is NewTask => {
 
   const task = body as Record<string, unknown>;
 
+  const hasInvalidField = Object.keys(task).some((key) => !allowedFields.includes(key));
+  if (hasInvalidField) return false;
+
   return (
     typeof task.title === 'string' &&
     task.title.trim() !== '' &&
     (task.status === 'todo' || task.status === 'doing' || task.status === 'done') &&
-    (task.term === undefined || typeof task.term === 'string')
+    (task.term === null || (typeof task.term === 'string' && task.term.trim() !== ''))
   );
 };
 
 export const parseTask = (input: unknown): NewTask => {
   if (!isNewTask(input)) throw new AppError('Invalid Task', HttpStatus.BAD_REQUEST, 'task');
+
+  return input;
+};
+
+export const isPatchTask = (body: unknown): body is TaskPatch => {
+  if (typeof body !== 'object' || body === null) return false;
+
+  const task = body as Record<string, unknown>;
+  const hasFields = Object.keys(task).length > 0;
+
+  const hasInvalidField = Object.keys(task).some((key) => !allowedFields.includes(key));
+  if (hasInvalidField) return false;
+
+  return (
+    hasFields &&
+    (task.title === undefined || (typeof task.title === 'string' && task.title.trim() !== '')) &&
+    (task.status === undefined ||
+      task.status === 'todo' ||
+      task.status === 'doing' ||
+      task.status === 'done') &&
+    (task.term === undefined ||
+      task.term === null ||
+      (typeof task.term === 'string' && task.term.trim() !== ''))
+  );
+};
+
+export const parsePatchTask = (input: unknown): TaskPatch => {
+  if (!isPatchTask(input)) throw new AppError('Invalid Task', HttpStatus.BAD_REQUEST, 'task');
 
   return input;
 };
