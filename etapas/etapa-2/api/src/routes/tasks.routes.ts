e8c0aad48911c65502e4035baa0d99995733960e
Fillip <fillip.apol@gmail.com>
2026-07-24 13:40:59 -0300

    Tipado rota patch e outras refatoracoes

--- etapas/etapa-2/api/src/routes/tasks.routes.ts
+++ etapas/etapa-2/api/src/routes/tasks.routes.ts
@@ -1,23 +1,23 @@
 import { type Request, type Response, type NextFunction, Router } from 'express';
 import { AppError } from '../errors.js';
 import { HttpStatus } from '../constants/http-constants.js';
-import { parseTask, type Task } from '../tasks.js';
+import { parsePatchTask, parseTask, type Task } from '../tasks.js';
 
 const tasksRoutes = Router();
 
-const tasks: Task[] = [
-  { id: 1, title: 'comprar pão', status: 'todo' },
-  { id: 2, title: 'estudar express', status: 'todo' },
-];
 let nextId = 3;
+const tasks: Task[] = [
+  { id: 1, title: 'comprar pão', status: 'todo', term: 'Teste' },
+  { id: 2, title: 'estudar express', status: 'todo', term: null },
+];
 
 // Para o teste
-// export const resetTasks = () => {
-//   tasks.length = 0;
-//   tasks.push({ id: 1, title: 'comprar pão', done: false });
-//   tasks.push({ id: 2, title: 'estudar express', done: true });
-//   nextId = 3;
-// };
+export const resetTasks = () => {
+  tasks.length = 0;
+  tasks.push({ id: 1, title: 'comprar pão', status: 'todo', term: 'oi' });
+  tasks.push({ id: 2, title: 'estudar express', status: 'todo', term: null });
+  nextId = 3;
+};
 
 // const validateTitle = (req: Request, res: Response, next: NextFunction) => {
 //   const title = req.body?.title;
@@ -47,7 +47,7 @@ tasksRoutes.get('/:id', validateId, (req: Request, res: Response, next: NextFunc
   const task = tasks.find((task) => task.id === req.taskId);
 
   if (typeof task === 'undefined') {
-    const err = new AppError('not found', 404, 'id');
+    const err = new AppError('Not Found', HttpStatus.NOT_FOUND, 'id');
     return next(err);
   }
 
@@ -58,26 +58,25 @@ tasksRoutes.post('/', (req: Request, res: Response, _next: NextFunction) => {
   const receivedTask = parseTask(req.body);
   const newTask: Task = { id: nextId++, ...receivedTask };
   tasks.push(newTask);
-  return res.status(201).location(`/tasks/${newTask.id}`).json(newTask);
+  return res.status(HttpStatus.CREATED).location(`/tasks/${newTask.id}`).json(newTask);
 });
 
-// tasksRoutes.patch(
-//   '/:id',
-//   validateId,
-//   validateTitle,
-//   (req: Request, res: Response, next: NextFunction) => {
-//     const task = tasks.find((task) => task.id === req.id);
+tasksRoutes.patch('/:id', validateId, (req: Request, res: Response, next: NextFunction) => {
+  const patchTask = parsePatchTask(req.body);
 
-//     if (typeof task === 'undefined') {
-//       const err = new AppError('not found', 404, 'id');
-//       return next(err);
-//     }
+  const task = tasks.find((task) => task.id === req.taskId);
 
-//     task.title = req.body.title;
+  if (typeof task === 'undefined') {
+    const err = new AppError('Not Found', HttpStatus.NOT_FOUND, 'id');
+    return next(err);
+  }
 
-//     return res.status(200).json(task);
-//   },
-// );
+  if (patchTask.title !== undefined) task.title = patchTask.title;
+  if (patchTask.status !== undefined) task.status = patchTask.status;
+  if (patchTask.term !== undefined) task.term = patchTask.term;
+
+  return res.json(task);
+});
 
 // tasksRoutes.delete('/:id', validateId, (req: Request, res: Response, next: NextFunction) => {
 //   const index = tasks.findIndex((task) => task.id === req.id);
