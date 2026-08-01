224d99d1c05798df4914a68300885a4a4708531a
Fillip <fillip.apol@gmail.com>
2026-07-31 23:37:15 -0300

    Atualizado root e app.tsx  para tailwind.

--- etapas/etapa-3/web/src/App.tsx
+++ etapas/etapa-3/web/src/App.tsx
@@ -1,14 +1,13 @@
 import { Header } from './components/Header';
 import { TaskSection } from './components/tasks/TaskSection';
 import { mockTasks } from './data/mockTasks';
-import './App.css';
 // import { emptyTask } from './data/mockTasks'; para testar com array vazio
 
 function App() {
   return (
     <>
       <Header />
-      <main>
+      <main className="flex w-screen justify-evenly">
         <TaskSection tasks={mockTasks} />
       </main>
       <footer></footer>
