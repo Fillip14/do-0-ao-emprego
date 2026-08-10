ea978d78bbd2fae5deac04b85264449da9c78d11
Fillip <fillip.apol@gmail.com>
2026-08-10 09:49:47 -0300

    Alteracao do card.

--- etapas/etapa-3/web/src/components/Card.tsx
+++ etapas/etapa-3/web/src/components/Card.tsx
@@ -1,8 +1,16 @@
-type CardProps = { children?: React.ReactNode };
+import { classNames } from '../utils/classNames';
 
-export const Card = ({ children }: CardProps) => {
+type CardProps = React.ComponentProps<'div'>;
+
+export const Card = ({ children, className, ...rest }: CardProps) => {
   return (
-    <div className="flex flex-col py-4 px-3 w-50 h-60 sm:w-100 sm:h-120 bg-amber-200 rounded-2xl shadow-lg overflow-y-auto overflow-hidden  scrollbar-gutter-both scrollbar-thin scrollbar-thumb-amber-600/40">
+    <div
+      className={classNames([
+        'flex flex-col py-4 px-3 w-50 h-60 sm:w-100 sm:h-120 bg-amber-200 rounded-2xl shadow-lg overflow-y-auto overflow-hidden scrollbar-gutter-both scrollbar-thin scrollbar-thumb-amber-600/40',
+        className ?? '',
+      ])}
+      {...rest}
+    >
       {children}
     </div>
   );
